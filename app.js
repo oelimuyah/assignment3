14cb212289b50ef5addf66ecae40fca390ec8f3d
Elimuyah O <oelimuyah@gmail.com>
2025-09-25 05:18:31 -0500

    Add new update

--- app.js
+++ app.js
@@ -1,21 +1,64 @@
-import chalk from 'chalk';//third party module
+// Built-in modules
+import { stdin } from 'process';
+
+// Third-party module
+import chalk from 'chalk';
+import path from 'node:path';
+// Custom modules
+import { add, subtract, multiply, divide } from './my_modules/calculator.js';
+import { formatUserInfo} from './formatInfo.js';
+
+let number1 = null;
+let number2 = null;
+let opp = ''; 
+
+console.log(chalk.bgBlue('BD/2025/TC3/104'));//display registration number
+const filename = path.basename(import.meta.url);//basename of current file
+console.log(chalk.bgGreen('Path',filename));
 
 
+console.log(chalk.red('Enter first number:'));//input for first number
+stdin.once('data', (Input) => {
+number1 = parseInt(String(Input));
 
-let myURL = import.meta.url;//
+console.log(chalk.red('Enter Second number:'));
+// Second input for second number
+stdin.once('data', (Input) => {
+number2 = parseInt(String(Input));
 
-let a = 10;
-let b = 3;
 
-import { add, subtract, multiply, divide } from './calculator.js';//local module
-import { url } from 'node:inspector';
+// Ask User to choose an operation
+console.log(chalk.red('Choose an operation?'));
+stdin.once('data', (Input) => {
+opp = String(Input).trim();
 
-console.log(chalk.bgYellow('Welcome to Calculator Application'));//third party module
-
-console.log(chalk.bgBlue('BD/2025/TC3/104'));
-console.log(chalk.bgGreen(myURL)); //core module
-
-console.log(chalk.blue('Addition: ' + add(a, b)));
-console.log(chalk.green('Subtraction: ' + subtract(a, b)));
-console.log(chalk.yellow('Multiplication: ' + multiply(a, b)));
-console.log(chalk.red('Division: ' + divide(a, b).toFixed(2)));
+switch (opp) {
+case "+":
+    const result = add(number1, number2);
+    console.log(chalk.blueBright("Add",formatUserInfo(number1, opp, number2, result)));;
+break;
+case "-":
+    const result1 = subtract(number1, number2);
+    console.log(chalk.blueBright("Subtract",formatUserInfo(number1, opp, number2, result1)));;
+break;
+case "*":
+    const result2 = multiply(number1, number2);
+    console.log(chalk.blueBright("Multiply",formatUserInfo(number1, opp, number2, result2)));;
+break;
+case "/":
+    if (number2 === 0) {
+        console.log(chalk.bgRed('Cannot divide by zero'));
+        process.exit();
+    }else
+    {
+    const result3 = divide(number1, number2);
+    console.log(chalk.blueBright("Divide",formatUserInfo(number1, opp, number2, result3)));;
+    }
+break;
+default:
+console.log(chalk.bgRed('Invalid Operation'));
+} 
+process.exit();
+})
+});
+});
---
+++ formatInfo.js
@@ -0,0 +1,3 @@
+export function formatUserInfo(number1, opp, number2, result) {
+return `Operation: ${number1} ${opp} ${number2} = ${result}.`;
+}
